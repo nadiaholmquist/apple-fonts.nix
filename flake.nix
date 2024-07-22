3e69e75119a98cc38cd2460eeb1f2c2df9453fa3
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-07-22 16:18:52 +0200

    Refactor to not use the fonts as flake inputs
    The inputs are always downloaded even if you don't use all the fonts,
    which feels wasteful to me.

--- flake.nix
+++ flake.nix
@@ -2,35 +2,41 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    sf-pro = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-      flake = false;
-    };
-    sf-compact = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-      flake = false;
-    };
-    sf-mono = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-      flake = false;
-    };
-    sf-arabic = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg";
-      flake = false;
-    };
-    ny = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
-      flake = false;
-    };
   };
-
-  outputs = { self, nixpkgs, flake-utils, sf-pro, sf-compact, sf-mono, sf-arabic, ny }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      unpackPhase = pkgName: ''
+      urlBase = "https://devimages-cdn.apple.com/design/resources/download/";
+      sf-pro = {
+        name = "sf-pro";
+        dmg = "SF-Pro.dmg";
+        hash = "sha256-B8xljBAqOoRFXvSOkOKDDWeYUebtMmQLJ8lF05iFnXk=";
+      };
+      sf-compact = {
+        name = "sf-compact";
+        dmg = "SF-Compact.dmg";
+        hash = "sha256-L4oLQ34Epw1/wLehU9sXQwUe/LaeKjHRxQAF6u2pfTo=";
+      };
+      sf-mono = {
+        name = "sf-mono";
+        dmg = "SF-Mono.dmg";
+        hash = "sha256-Uarx1TKO7g5yVBXAx6Yki065rz/wRuYiHPzzi6cTTl8=";
+      };
+      sf-arabic = {
+        name = "sf-arabic";
+        dmg = "SF-Arabic.dmg";
+        hash = "sha256-1clBp+aePSLNR9JrS+jReH7pEJtsH+zpzsiBKLQvvUs=";
+      };
+      ny = {
+        name = "ny";
+        dmg = "NY.dmg";
+        hash = "sha256-yYyqkox2x9nQ842laXCqA3UwOpUGyIfUuprX975OsLA=";
+      };
+      unpackPhase = dmg: ''
+        ls
+        echo $src
         undmg $src
-        7z x '${pkgName}'
+        7z x *.pkg
         7z x 'Payload~'
       '';
       commonInstall = ''
@@ -39,10 +45,14 @@
         mkdir -p $out/share/fonts/truetype
       '';
       commonBuildInputs = with pkgs; [ undmg p7zip ];
-      makeAppleFont = (name: pkgName: src: pkgs.stdenvNoCC.mkDerivation {
-        inherit name src;
+      makeAppleFont = (font: pkgs.stdenvNoCC.mkDerivation {
+        name = font.name;
+        src = pkgs.fetchurl {
+          url = "${urlBase}${font.dmg}";
+          hash = font.hash;
+        };
 
-        unpackPhase = unpackPhase pkgName;
+        unpackPhase = unpackPhase font.dmg;
 
         buildInputs = commonBuildInputs;
         setSourceRoot = "sourceRoot=`pwd`";
@@ -52,39 +62,13 @@
           find -name \*.ttf -exec mv {} $out/share/fonts/truetype/ \;
         '';
       });
-      makeNerdAppleFont = (name: pkgName: src: pkgs.stdenvNoCC.mkDerivation {
-        inherit name src;
-
-        unpackPhase = unpackPhase pkgName;
-
-        buildInputs = with pkgs; commonBuildInputs ++ [ parallel nerd-font-patcher ];
-        setSourceRoot = "sourceRoot=`pwd`";
-
-        buildPhase = ''
-          find -name \*.ttf -o -name \*.otf -print0 | parallel -j $NIX_BUILD_CORES -0 nerd-font-patcher -c {}
-        '';
-
-        installPhase = commonInstall + ''
-          find -name \*.otf -maxdepth 1 -exec mv {} $out/share/fonts/opentype/ \;
-          find -name \*.ttf -maxdepth 1 -exec mv {} $out/share/fonts/truetype/ \;
-        '';
-      });
-    in rec {
+    in {
       packages = {
-        sf-pro = makeAppleFont "sf-pro" "SF Pro Fonts.pkg" sf-pro;
-        sf-pro-nerd = makeNerdAppleFont "sf-pro-nerd" "SF Pro Fonts.pkg" sf-pro;
-
-        sf-compact = makeAppleFont "sf-compact" "SF Compact Fonts.pkg" sf-compact;
-        sf-compact-nerd = makeNerdAppleFont "sf-compact-nerd" "SF Compact Fonts.pkg" sf-compact;
-
-        sf-mono = makeAppleFont "sf-mono" "SF Mono Fonts.pkg" sf-mono;
-        sf-mono-nerd = makeNerdAppleFont "sf-mono-nerd" "SF Mono Fonts.pkg" sf-mono;
-
-        sf-arabic = makeAppleFont "sf-arabic" "SF Arabic Fonts.pkg" sf-arabic;
-        sf-arabic-nerd = makeNerdAppleFont "sf-arabic-nerd" "SF Arabic Fonts.pkg" sf-arabic;
-
-        ny = makeAppleFont "ny" "NY Fonts.pkg" ny;
-        ny-nerd = makeNerdAppleFont "ny-nerd" "NY Fonts.pkg" ny;
+        sf-pro = makeAppleFont sf-pro;
+        sf-compact = makeAppleFont sf-compact;
+        sf-mono = makeAppleFont sf-mono;
+        sf-arabic = makeAppleFont sf-arabic;
+        ny = makeAppleFont ny;
       };
     }
   );
