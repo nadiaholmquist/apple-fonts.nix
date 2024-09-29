d6e0db018432c537941834716b4f48011abaef26
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-09-29 05:08:44 +0200

    significantly clean up the flake

--- flake.nix
+++ flake.nix
@@ -1,75 +1,63 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      urlBase = "https://devimages-cdn.apple.com/design/resources/download/";
+
+  outputs = { nixpkgs, ... }: let
+    inherit (nixpkgs.lib) genAttrs mapAttrs systems;
+
+    fonts = {
       sf-pro = {
-        name = "sf-pro";
         dmg = "SF-Pro.dmg";
         hash = "sha256-IccB0uWWfPCidHYX6sAusuEZX906dVYo8IaqeX7/O88";
       };
       sf-compact = {
-        name = "sf-compact";
         dmg = "SF-Compact.dmg";
         hash = "sha256-PlraM6SwH8sTxnVBo6Lqt9B6tAZDC//VCPwr/PNcnlk";
       };
       sf-mono = {
-        name = "sf-mono";
         dmg = "SF-Mono.dmg";
         hash = "sha256-bUoLeOOqzQb5E/ZCzq0cfbSvNO1IhW1xcaLgtV2aeUU";
       };
       sf-arabic = {
-        name = "sf-arabic";
         dmg = "SF-Arabic.dmg";
         hash = "sha256-J2DGLVArdwEsSVF8LqOS7C1MZH/gYJhckn30jRBRl7k";
       };
       ny = {
-        name = "ny";
         dmg = "NY.dmg";
         hash = "sha256-HC7ttFJswPMm+Lfql49aQzdWR2osjFYHJTdgjtuI+PQ";
       };
-      unpackPhase = dmg: ''
-        ls
-        echo $src
-        undmg $src
-        7z x *.pkg
-        7z x 'Payload~'
-      '';
-      commonInstall = ''
-        mkdir -p $out/share/fonts
-        mkdir -p $out/share/fonts/opentype
-        mkdir -p $out/share/fonts/truetype
-      '';
-      commonBuildInputs = with pkgs; [ undmg p7zip ];
-      makeAppleFont = (font: pkgs.stdenvNoCC.mkDerivation {
-        name = font.name;
+    };
+  in {
+    packages = genAttrs systems.flakeExposed (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      makeAppleFont = name: font: pkgs.stdenvNoCC.mkDerivation {
+        inherit name;
+
         src = pkgs.fetchurl {
-          url = "${urlBase}${font.dmg}";
+          url = "https://devimages-cdn.apple.com/design/resources/download/${font.dmg}";
           hash = font.hash;
         };
 
-        unpackPhase = unpackPhase font.dmg;
+        nativeBuildInputs = with pkgs; [
+          undmg
+          p7zip
+        ];
 
-        buildInputs = commonBuildInputs;
-        setSourceRoot = "sourceRoot=`pwd`";
+        unpackPhase = ''
+          undmg $src
+          7z x *.pkg
+          7z x 'Payload~'
+        '';
 
-        installPhase = commonInstall + ''
+        installPhase = ''
+          mkdir -p $out/share/fonts/opentype
+          mkdir -p $out/share/fonts/truetype
           find -name \*.otf -exec mv {} $out/share/fonts/opentype/ \;
           find -name \*.ttf -exec mv {} $out/share/fonts/truetype/ \;
         '';
-      });
-    in {
-      packages = {
-        sf-pro = makeAppleFont sf-pro;
-        sf-compact = makeAppleFont sf-compact;
-        sf-mono = makeAppleFont sf-mono;
-        sf-arabic = makeAppleFont sf-arabic;
-        ny = makeAppleFont ny;
       };
-    }
-  );
+    in
+      mapAttrs makeAppleFont fonts);
+  };
 }
